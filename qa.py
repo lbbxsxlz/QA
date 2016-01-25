941d0880c196f779177faf6f0891236193cdf035
Chaohong Hu <huchhhong@gmail.com>
2016-01-25 18:09:21 +0800

    增加关键字抽取结果展示

--- qa.py
+++ qa.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import jieba
+import jieba.analyse
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -10,13 +11,21 @@ def root():
     return app.send_static_file('index.html')
 
 
-@app.route('/answer', methods=['POST'])
-def answer():
+@app.route('/wordsplit', methods=['POST'])
+def wordsplit():
     question = request.form['q'].encode('utf8')
     sep_list = jieba.cut(question)
     result = '/ '.join(sep_list).encode('utf8')
-    return "<b>问句</b> " + question + " <b>分词结果:</b> " + result
+    return "<b>问句</b> " + question + " <br/><b>分词结果:</b> " + result
+
+
+@app.route('/keyword', methods=['POST'])
+def keyword():
+    question = request.form['q'].encode('utf8')
+    tags = jieba.analyse.extract_tags(question)
+    result = ','.join(tags).encode('utf8')
+    return "<b>问句</b> " + question + "<br/><b>关键字:</b> " + result
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
