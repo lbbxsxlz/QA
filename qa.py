f8c5ec7a5482043254f9b053d2b866d1c033ae60
Chaohong Hu <huchhhong@gmail.com>
2016-01-25 18:38:16 +0800

    增加词性标注展示

--- qa.py
+++ qa.py
@@ -2,6 +2,8 @@
 
 import jieba
 import jieba.analyse
+import jieba.posseg as pseg
+
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -27,6 +29,17 @@ def keyword():
     for tag in tags:
         result += "{}({}), ".format(tag[0].encode('utf8'),
                                     tag[1])
+    return result
+
+
+@app.route('/pos', methods=['POST'])
+def pos():
+    question = request.form['q'].encode('utf8')
+    words = pseg.cut(question)
+    result = "<b>问句</b> " + question + "<br/><b>词性标注:</b>"
+    for word, flag in words:
+        word = word.encode('utf8')
+        result += "{} {}, ".format(word, flag)
     return result
 
 if __name__ == "__main__":
